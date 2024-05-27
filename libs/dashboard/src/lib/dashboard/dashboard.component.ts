0adfc096fd19960d7b8bc697453334830925042c
phatnh13 <phatnguyenhoang69@gmail.com>
2024-05-27 12:04:18 +0700

    example router link

--- libs/dashboard/src/lib/dashboard/dashboard.component.ts
+++ libs/dashboard/src/lib/dashboard/dashboard.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'lib-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
 export class DashboardComponent {
+  public routerLinkVariable = '/home';
 }
